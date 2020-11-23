1e9386160e5d20cf5b929f1d1d3cf12ac1589a53
pXius <fan.engelbrecht@gmail.com>
2020-11-23 16:20:20 +0100

    First Commit

---
+++ src/test/java/se/kth/sda/ta/TeachingAssistantTests.java
@@ -0,0 +1,13 @@
+package se.kth.sda.ta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachingAssistantTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
