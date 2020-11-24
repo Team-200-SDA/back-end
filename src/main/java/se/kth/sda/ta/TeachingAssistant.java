ef1c2197410702f0404c476b24da01cf1a4093a5
pXius <fan.engelbrecht@gmail.com>
2020-11-24 08:55:54 +0100

    New Additions:
    Websocket Event Listene.
    New Chat Controller for Websockets.
    New Chat Message Model for Websocket Chat.
    Websocket Config created with /ws endpoints, and CORS setup for localhost:3000. This will be for dev cycle only.
    User Controller created to return username and email on GET
    Modified Websecurity config to allow ws/** requests.

--- src/main/java/se/kth/sda/ta/TeachingAssistant.java
+++ src/main/java/se/kth/sda/ta/TeachingAssistant.java
@@ -9,20 +9,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class TeachingAssistant {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TeachingAssistant.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TeachingAssistant.class, args);
+    }
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry
-					.addMapping("/**")
-					.allowedOrigins("*")
-					.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-			}
-		};
-	}
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
 }
