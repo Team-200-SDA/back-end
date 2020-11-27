435a6193405a714595dbc5a9b4d8ab24000be38f
Radha <radhasr1986@gmail.com>
2020-11-27 15:19:43 +0100

    implementation for userprofile

--- src/main/java/se/kth/sda/ta/user/UserService.java
+++ src/main/java/se/kth/sda/ta/user/UserService.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service()
 public class UserService {
     @Autowired
@@ -21,4 +23,18 @@ public class UserService {
         user.setPassword(encryptedPass);
         userRepository.save(user);
     }
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User updateAddress(User updatedUser) {
+        return userRepository.save(updatedUser);
+    }
+    public User updatePhoneno(User updatedUser) {
+        return userRepository.save(updatedUser);
+    }
+    public User updateProfilepic(User updatedUser) {
+        return userRepository.save(updatedUser);
+    }
+
 }
