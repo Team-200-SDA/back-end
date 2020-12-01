40ef12ecb91df56955b450dca3bd2f6eb84ea1c6
pXius <fan.engelbrecht@gmail.com>
2020-12-01 11:05:13 +0100

    Updated Assignment Functionality to link up with User profiles. Each assignment will now have a User to validate roles. Created new enpoints to return certain assignment objects based on user roles

--- src/main/java/se/kth/sda/ta/assignment/AssignmentRepository.java
+++ src/main/java/se/kth/sda/ta/assignment/AssignmentRepository.java
@@ -2,8 +2,13 @@ package se.kth.sda.ta.assignment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import se.kth.sda.ta.user.User;
+
+import java.util.List;
 
 @Repository
 public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
+    List<Assignment> findAllByUser(User user);
+    List<Assignment> findAllByUser_Role(String role);
 
 }
