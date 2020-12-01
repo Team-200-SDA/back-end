1755d99163b3443d6e6fcc8edf189a1900c94b61
Sneha <sneha.gupta@gmail.com>
2020-12-01 23:16:11 +0100

    Undo last commit(Wrong branch)

--- src/main/java/se/kth/sda/ta/news/NewsRepository.java
+++
@@ -1,9 +0,0 @@
-package se.kth.sda.ta.news;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NewsRepository extends JpaRepository<News, Long> {
-
-}
