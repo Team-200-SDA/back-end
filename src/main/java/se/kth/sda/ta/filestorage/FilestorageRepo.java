69e9badf4f0f1baab288cfab5012216e5a69a2c6
Radha <radhasr1986@gmail.com>
2020-12-02 14:10:53 +0100

    filestorage for single user implemented

--- src/main/java/se/kth/sda/ta/filestorage/FilestorageRepo.java
+++ src/main/java/se/kth/sda/ta/filestorage/FilestorageRepo.java
@@ -2,8 +2,11 @@ package se.kth.sda.ta.filestorage;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import se.kth.sda.ta.user.User;
+
+import java.util.List;
 
 @Repository
 public interface FilestorageRepo extends JpaRepository<Filestorage, Long> {
-
+    List<Filestorage> findAllByUser(User user);
 }
