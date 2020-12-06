d71211cf1f9fb11dd8bf0c4d1048962f090dbc9f
pXius <fan.engelbrecht@gmail.com>
2020-12-06 21:58:49 +0100

    Modified Lecture model to be in line with File storage Modified Lectuer controller to add a uder to each lecture object.

--- src/main/java/se/kth/sda/ta/globalchat/controller/WebSocketEventListener.java
+++ src/main/java/se/kth/sda/ta/globalchat/controller/WebSocketEventListener.java
@@ -6,16 +6,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 import se.kth.sda.ta.globalchat.model.Chat;
 
+@Component
 public class WebSocketEventListener {
 
     private static final Logger logger = LoggerFactory.getLogger(WebSocketEventListener.class);
 
+
+    private final SimpMessageSendingOperations messagingTemplate;
+
     @Autowired
-    private SimpMessageSendingOperations messagingTemplate;
+    public WebSocketEventListener(SimpMessageSendingOperations messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
 
     @EventListener
     public void handleWebSocketConnectListener(SessionConnectedEvent event) {
