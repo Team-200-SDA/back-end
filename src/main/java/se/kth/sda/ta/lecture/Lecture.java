d12063e30e552272812529974dc884113fdad299
pXius <fan.engelbrecht@gmail.com>
2020-12-07 12:29:11 +0100

    Changed Lecture and Assignment back to normal layout. Inheritance causing files to be available in other tables.

--- src/main/java/se/kth/sda/ta/lecture/Lecture.java
+++ src/main/java/se/kth/sda/ta/lecture/Lecture.java
@@ -1,12 +1,81 @@
 package se.kth.sda.ta.lecture;
 
-import se.kth.sda.ta.filestorage.Filestorage;
+import se.kth.sda.ta.user.User;
 
 import javax.persistence.*;
 
 @Entity
-@Table
-public class Lecture extends Filestorage {
-// This table structure essentially identical to the Filestorage class.
-// Hence the inheritance implementation.
+@Table(name = "lectures")
+public class Lecture {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fileName;
+    private String link;
+    private String publicId;
+    private String type;
+
+    @ManyToOne
+    private User user;
+
+    public Lecture(Long id, String fileName, String link, String publicId, String type, User user) {
+        this.id = id;
+        this.fileName = fileName;
+        this.link = link;
+        this.publicId = publicId;
+        this.type = type;
+        this.user = user;
+    }
+
+    public Lecture() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getPublicId() {
+        return publicId;
+    }
+
+    public void setPublicId(String publicId) {
+        this.publicId = publicId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
