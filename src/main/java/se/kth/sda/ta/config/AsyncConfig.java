6296a8a5a8aa8918ad17002e6a7aab751aaf96c1
pXius <fan.engelbrecht@gmail.com>
2020-12-17 17:20:20 +0100

    Async Settings and application.properties update

--- src/main/java/se/kth/sda/ta/config/AsyncConfig.java
+++ src/main/java/se/kth/sda/ta/config/AsyncConfig.java
@@ -1,11 +1,13 @@
 package se.kth.sda.ta.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableAsync
 public class AsyncConfig implements WebMvcConfigurer {
 
     @Override
